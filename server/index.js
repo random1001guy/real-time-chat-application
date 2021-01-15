08f69482326e52e436ccfb3a9ad7851fcc149cc8
random1001guy <random1001guy@gmail.com>
2021-01-15 22:37:55 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
